f0d44bbe87d7a584f7edcffe733667dd5f9a700d
Milosz Olejnik <milosz.olejnik@tuta.io>
2022-01-23 18:27:36 +0100

    Render posts

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,30 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
+import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan, faPenFancy } from '@fortawesome/free-solid-svg-icons'
 import { getBlogsAction } from '../actions/getBlogsAction';
 import { getPostsAction } from "../actions/getPostsAction";
 
 function MainPage() {
-
-    //Featch Blogs Names
     const dispatch = useDispatch();
+    //Featch Blogs Names
     useEffect(() => {
-        dispatch(getBlogsAction())
+        dispatch(getBlogsAction());
     }, []);
+
+    const [postRender, setPostRender] = useState(false);
+    const [postsArray, setPostsArray] = useState([null]);
 
     //Keys for blogs names
     let i = 0
 
     //Define state
     const BlogsState = useSelector(state => state.getBlogsReducer);
-    // const getPosts = dispatch(getPostsAction(BlogsState.names[0]));
+    const BlogsPosts = useSelector(state => state.getPostsReducer);
+
+    function getPosts() {
+
+        const postList = BlogsPosts.post;
+
+        if (BlogsPosts.isLoading) {
+            console.log('loading status = ' + BlogsPosts.isLoading)
+            setPostRender(false);
+        } else {
+            if (typeof (postList) === 'string') {
+                setPostRender(false);
+                console.log(' empty')
+            } else {
+                setPostsArray(postList.items);
+                setPostRender(true);
+            }
+        }
+    }
+
     //Blog Secelt
     const blogList = (e) => {
         const blogName = e.target.value;
-        dispatch(getPostsAction(blogName));
+        dispatch(getPostsAction(blogName)).then(getPosts());
     }
 
-    //Post state
-    const postState = useSelector(state => state.getPostsReducer);
 
     //Render posts
     return (
@@ -37,10 +59,50 @@ function MainPage() {
                 })}
             </select>
             <div>
-                {postState.isLoading ? null : null}
+                {postRender ? postsArray.map(post => {
+                    return <StyledPostContainer key={post.id}>
+                        <div>
+                            <StyledDateAndTitle>
+                                <StyledTitle>{post.postTitle}</StyledTitle>
+                                <p>{post.createDate}</p>
+                            </StyledDateAndTitle>
+                            <div>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, laborum.</p>
+                            </div>
+                        </div>
+                        <StyledIcons>
+                            <FontAwesomeIcon icon={faTrashCan} />
+                            <FontAwesomeIcon icon={faPenFancy} />
+                        </StyledIcons>
+                    </StyledPostContainer>
+                }) : console.log(postRender)}
             </div>
         </div >
     )
 }
-
+const StyledPostContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    box-shadow: 0px 4px 10px 5px rgba(0, 0, 0, 0.05);
+`
+const StyledDateAndTitle = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+const StyledTitle = styled.p`
+    font-weight: bold;
+    cursor: pointer;
+`
+const StyledIcons = styled.div`
+    display: flex;
+    align-items: center;
+        svg{
+            height: 25px;
+            margin-left:1rem;
+            cursor: pointer;
+        }
+`
 export default MainPage
